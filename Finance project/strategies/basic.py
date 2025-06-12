7ff6f72b86ddab20feb8093d34e6fd2cdb4df3b5
sudheendrarao7 <ananicharitha@gmail.com>
2025-06-12 10:09:56 +0530

    Update basic.py

--- Finance project/strategies/basic.py
+++ Finance project/strategies/basic.py
@@ -3,25 +3,16 @@
 import numpy as np
 
 def long_call(prices, strike, premium):
-    """
-    Payoff = max(S - K, 0) - Premium
-    """
     return np.maximum(prices - strike, 0) - premium
 
 def short_call(prices, strike, premium):
-    """
-    Payoff = -max(S - K, 0) + Premium
-    """
+    
     return -1 * (np.maximum(prices - strike, 0) - premium)
 
 def long_put(prices, strike, premium):
-    """
-    Payoff = max(K - S, 0) - Premium
-    """
+    
     return np.maximum(strike - prices, 0) - premium
 
 def short_put(prices, strike, premium):
-    """
-    Payoff = -max(K - S, 0) + Premium
-    """
+    
     return -1 * (np.maximum(strike - prices, 0) - premium)
